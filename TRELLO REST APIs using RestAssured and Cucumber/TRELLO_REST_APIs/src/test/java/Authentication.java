b92a208aa146792531f30e5ed4ad08fa84f8874c
rana-ai <123319582+rana-ai@users.noreply.github.com>
2023-02-19 02:52:02 +0200

    Add files via upload

---
+++ TRELLO REST APIs using RestAssured and Cucumber/TRELLO_REST_APIs/src/test/java/Authentication.java
@@ -0,0 +1,12 @@
+import java.sql.Statement;
+
+public class Authentication {
+    public static String apiKey = "3510215304a0da1b8cbc00df7502670e" ;
+
+    public static String apiToken = "ATTAfe02758adc679416649912e11dfda88514989057269e35549d843757967a540b1641F751" ;
+
+
+
+
+
+}
